55b7d8d117fabdae3dce42d4cd816a330813c9d9
mc007 <asd28071977>
2015-06-03 17:02:44 +0200

    =split into item: reference , source and instance

--- data/Reference.js
+++ data/Reference.js
@@ -1,8 +1,25 @@
+/** @module xgrid/data/Reference **/
 define([
     "xdojo/declare"
 ], function (declare) {
 
+    /**
+     * A grid item feature
+     * @class module:xgrid/data/Reference
+     */
     var Implementation = {
+
+        _sources:[],
+        addSource:function(source){},
+        removeSource:function(source){},
+
+        updateSource:function(sources){},
+        updateSources:function(sources){},
+
+        onSourceUpdate:function(source){},
+
+        onSourceRemoved:function(source){},
+        onSourceDelete:function(source){}
 
     };
     //package via declare
