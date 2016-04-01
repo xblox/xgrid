6bf0c224eb7e88a813364026c72e8437ecf970f8
mc007 <asd28071977>
2016-04-01 03:23:16 +0200

    =multi expand for tree added

--- TreeRenderer.js
+++ TreeRenderer.js
@@ -1,4 +1,4 @@
-/** @module xgrid/ThumbRenderer **/
+/** @module xgrid/TreeRenderer **/
 define([
     "xdojo/declare",
     './Renderer',
@@ -7,19 +7,134 @@ define([
     "xide/utils",
     "dojo/on"
 ], function (declare,Renderer,Tree,keys,utils,on) {
-
     var _debug = false;
+
+    function KEYBOARD_HANDLER(evt){
+        this.onTreeKey(evt);
+        var thiz = this;
+        if(thiz.isThumbGrid){
+            return;
+        }
+        if(evt.keyCode==keys.LEFT_ARROW ||evt.keyCode==keys.RIGHT_ARROW || evt.keyCode==keys.HOME || evt.keyCode==keys.END){
+        }else{
+            return;
+        }
+        if((evt.originalTarget && evt.originalTarget.className.indexOf('InputInner') != -1)){
+            return;
+        }
+        if((evt.target && evt.target.className.indexOf('input') != -1)){
+            //return;
+        }
+        var row = this.row(evt);
+        if(!row || !row.data){
+            return;
+        }
+        var data = row.data,
+            isExpanded = this._isExpanded(data),
+            store = this.collection,
+            storeItem = store.getSync(data[store.idProperty]);
+        //var children = data.getChildren ? data.getChildren() :  storeItem && storeItem.children ? null : store.children ? store.children(storeItem) : null;
+        var children = data.getChildren ? data.getChildren() :  storeItem && storeItem.children ? storeItem.children : null;
+        var isFolder = storeItem ? (storeItem.isDir || storeItem.directory) : false,
+            firstChild = children ? children[0] : false,
+            focused = this._focusedNode,
+            last = focused ? this.down(focused, children ? children.length : 0, true) :null,
+            loaded = ( storeItem._EX === true || storeItem._EX == null ),
+            selection = this.getSelection ? this.getSelection () : [storeItem],
+            next = null,
+            down = this.down(this._focusedNode, -1, true),
+            up = this.down(this._focusedNode, 1, true),
+            defaultSelectArgs = {
+                focus: true,
+                append: false,
+                delay: 1
+            };
+
+        if(firstChild && firstChild._reference){
+            var _b = store.getSync(firstChild._reference);
+            if(_b){
+                firstChild = _b;
+            }
+        }
+        if(evt.keyCode==keys.END) {
+            if(isExpanded && isFolder && last && last.element !==focused){
+                this.select([last.data], null, true, defaultSelectArgs);
+                return;
+            }
+        }
+
+        function expand(what,expand){
+            _.each(what, function (item) {
+                thiz.expand(thiz.row(item), expand, true);
+            });
+        }
+
+        if(evt.keyCode==keys.LEFT_ARROW){
+
+            evt.preventDefault();
+            if (data[store.parentField]){
+                var item = row.data;
+                if (!isExpanded) {
+                    var parent = store.getSync(item[store.parentField]);
+                    var parentRow = parent ? this.row(parent) : null;
+                    //we select the parent only if its rendered at all
+                    if (parent && parentRow.element ) {
+                        return this.select([parent], null, true, defaultSelectArgs);
+                    }else{
+                        if(down) {
+                            return this.select(down, null, true, defaultSelectArgs);
+                        }else {
+                            on.emit(this.contentNode, "keydown", {keyCode: 36, force: true});
+                        }
+                    }
+                }
+            }
+            if (row) {
+                if(isExpanded) {
+                    expand(selection,false);
+                }else{
+                    this.select(down, null, true, defaultSelectArgs);
+                }
+            }
+        }
+
+        if(evt.keyCode==keys.RIGHT_ARROW){
+            evt.preventDefault();
+            // empty folder:
+            if(isFolder && loaded && isExpanded && !firstChild){
+                //go to next
+                if(up) {
+                    return this.select(up, null, true, defaultSelectArgs);
+                }
+            }
+
+            if(loaded && isExpanded){
+                if(firstChild) {
+                    return this.select([firstChild], null, true, defaultSelectArgs);
+                }
+            }else{
+                //has children or not loaded yet
+                if(firstChild || !loaded || isFolder) {
+                    expand(selection,true);
+                }else{
+                    //case on an cell without no children: select do
+                    next = this.down(this._focusedNode, 1, true);
+                    if(next) {
+                        return this.select(next, null, true, defaultSelectArgs);
+                    }
+                }
+            }
+        }
+    }
     /**
-     * The list renderer does nothing since the xgrid/Base is already inherited from
-     * dgrid/OnDemandList and its rendering as list already.
      *
-     * @class module:xgrid/ThumbRenderer
+     * @class module:xgrid/TreeRenderer
      * @extends module:xgrid/Renderer
      */
-
     var Implementation = {
         _getLabel:function(){ return "Tree"; },
         _getIcon:function(){ return "fa-tree"; },
+        deactivateRenderer:function(renderer){},
         activateRenderer:function(renderer){
             this._showHeader(true);
         },
@@ -38,10 +153,8 @@ define([
             return item;
         },
         getCurrentFolder:function(){
-            var firstRow = this.getRows()[0];
-            return this.__getParent(firstRow);
+            return this.__getParent(this.getRows()[0]);
         },
-        deactivateRenderer:function(renderer){},
         _toFocusNode: function (item) {
             var row = this.row(item);
             if (row && row.element) {
@@ -73,131 +186,13 @@ define([
                 return;
             }
             this.inherited(arguments);
-            var thiz = this;
-
-
-            this.on("keydown", function (evt) {
-                this.onTreeKey(evt);
-                if(thiz.isThumbGrid){
-                    return;
-                }
-                if(evt.keyCode==keys.LEFT_ARROW ||evt.keyCode==keys.RIGHT_ARROW || evt.keyCode==keys.HOME || evt.keyCode==keys.END){
-                }else{
-                    return;
-                }
-                if((evt.originalTarget && evt.originalTarget.className.indexOf('InputInner') != -1)){
-                    return;
-                }
-                if((evt.target && evt.target.className.indexOf('input') != -1)){
-                    //return;
-                }
-                var row = this.row(evt);
-                if(!row || !row.data){
-                    return;
-                }
-                var data = row.data,
-                    isExpanded = this._isExpanded(data),
-                    store = this.collection,
-                    storeItem = store.getSync(data[store.idProperty]);
-                    //var children = data.getChildren ? data.getChildren() :  storeItem && storeItem.children ? null : store.children ? store.children(storeItem) : null;
-                    var children = data.getChildren ? data.getChildren() :  storeItem && storeItem.children ? storeItem.children : null;
-                    var isFolder = storeItem ? (storeItem.isDir || storeItem.directory) : false,
-                    firstChild = children ? children[0] : false,
-                    focused = this._focusedNode,
-                    last = focused ? this.down(focused, children ? children.length : 0, true) :null,
-                    loaded = ( storeItem._EX === true || storeItem._EX == null ),
-                    selection = this.getSelection ? this.getSelection () : [storeItem],
-                    next = null,
-                    down = this.down(this._focusedNode, -1, true),
-                    up = this.down(this._focusedNode, 1, true),
-                    defaultSelectArgs = {
-                        focus: true,
-                        append: false,
-                        delay: 1
-                    };
-
-                if(firstChild && firstChild._reference){
-                    var _b = store.getSync(firstChild._reference);
-                    if(_b){
-                        firstChild = _b;
-                    }
-                }
-                if(evt.keyCode==keys.END) {
-                    if(isExpanded && isFolder && last && last.element !==focused){
-                        this.select([last.data], null, true, defaultSelectArgs);
-                        return;
-                    }
-                }
-
-                function expand(what,expand){
-                    _.each(what, function (item) {
-                        thiz.expand(thiz.row(item), expand, true);
-                    });
-                }
-
-                if(evt.keyCode==keys.LEFT_ARROW){
-
-                    evt.preventDefault();
-                    if (data[store.parentField]){
-                        var item = row.data;
-                        if (!isExpanded) {
-                            var parent = store.getSync(item[store.parentField]);
-                            var parentRow = parent ? this.row(parent) : null;
-                            //we select the parent only if its rendered at all
-                            if (parent && parentRow.element ) {
-                                return this.select([parent], null, true, defaultSelectArgs);
-                            }else{
-                                if(down) {
-                                    return this.select(down, null, true, defaultSelectArgs);
-                                }else {
-                                    on.emit(this.contentNode, "keydown", {keyCode: 36, force: true});
-                                }
-                            }
-                        }
-                    }
-                    if (row) {
-                        if(isExpanded) {
-                            expand(selection,false);
-                        }else{
-                            this.select(down, null, true, defaultSelectArgs);
-                        }
-                    }
-                }
-
-                if(evt.keyCode==keys.RIGHT_ARROW){
-                    evt.preventDefault();
-                    // empty folder:
-                    if(isFolder && loaded && isExpanded && !firstChild){
-                        //go to next
-                        if(up) {
-                            return this.select(up, null, true, defaultSelectArgs);
-                        }
-                    }
-
-                    if(loaded && isExpanded){
-                        if(firstChild) {
-                            return this.select([firstChild], null, true, defaultSelectArgs);
-                        }
-                    }else{
-                        //has children or not loaded yet
-                        if(firstChild || !loaded || isFolder) {
-                            expand(selection,true);
-                        }else{
-                            //case on an cell without no children: select do
-                            next = this.down(this._focusedNode, 1, true);
-                            if(next) {
-                                return this.select(next, null, true, defaultSelectArgs);
-                            }
-                        }
-                    }
-                }
-            }.bind(this));
+            this.on("keydown",KEYBOARD_HANDLER.bind(this));
         }
     };
 
     //package via declare
-    var _class = declare('xgrid.TreeRenderer',[Renderer,Tree],Implementation);
-    _class.Implementation = Implementation;
-
-    return _class;
+    var Module = declare('xgrid.TreeRenderer',[Renderer,Tree],Implementation);
+    Module.Implementation = Implementation;
+    Module.KEYBOARD_HANDLER = KEYBOARD_HANDLER;
+    return Module;
 });
