fe3af2f71f992f62e0e2ae34d1f34c0fe97b26f7
mc007 <asd28071977>
2016-11-21 17:46:43 +0100

    auto-commit

---
+++ GridLite.js
@@ -0,0 +1,26 @@
+/** @module xgrid/Grid **/
+define([
+    'dojo/_base/declare',
+    'xide/types',
+    './BaseLite'
+],function (declare,types,Base) {
+    /**
+     *
+     * Please read {@link module:xgrid/types}
+     *
+     * @class module:xgrid/Grid
+     * @augments module:xgrid/Base
+     */
+    var grid = declare('xgrid/Grid',Base,{});
+
+    grid.createGridClass = Base.createGridClass;
+
+    //track defaults on module
+    grid.classFactory = Base.classFactory;
+    grid.DEFAULT_GRID_FEATURES = types.DEFAULT_GRID_FEATURES;
+    grid.DEFAULT_GRID_BASES = Base.DEFAULT_GRID_BASES;
+    grid.DEFAULT_GRID_OPTIONS = types.DEFAULT_GRID_OPTIONS;
+    grid.DEFAULT_GRID_OPTION_KEYS = types.DEFAULT_GRID_OPTION_KEYS;
+
+    return grid;
+});
---
+++ typesLite.js
@@ -0,0 +1,185 @@
+/** @module xgrid/types **/
+define([
+    "xdojo/declare",
+    'xide/types',
+    'xgrid/Selection',
+    'xgrid/Keyboard',
+    'xgrid/ColumnHider',
+    'xide/mixins/EventedMixin',
+    'dgrid/OnDemandGrid',
+    'xgrid/Defaults',
+    'xgrid/Layout',
+    'xgrid/Focus',
+    'xgrid/ListRenderer',
+    'xgrid/Clipboard',
+    'xgrid/Actions',
+    'xlang/i18'
+], function (declare,types,
+             Selection,_GridKeyboardSelection,ColumnHider,
+             EventedMixin, OnDemandGrid,Defaults,Layout,Focus,
+             ListRenderer,
+             Clipboard,Actions,i18)
+{
+    /**
+     * Grid Bases
+     * @enum module:xgrid/types/GRID_BASES
+     * @memberOf module:xgrid/types
+     */
+    types.GRID_BASES = {
+        GRID: OnDemandGrid,
+        LAYOUT:Layout,
+        DEFAULTS: Defaults,
+        RENDERER: ListRenderer,
+        EVENTED: EventedMixin,
+        FOCUS:Focus,
+        i18:i18
+    };
+    /**
+     * Default Grid Options
+     * @deprecated
+     * @enum module:xgrid/types/DEFAULT_GRID_OPTIONS
+     * @memberOf module:xgrid/types
+     */
+    types.DEFAULT_GRID_OPTIONS = {
+        /**
+         * Instruct the grid to add jQuery theme classes
+         * @default true
+         * @type {bool}
+         * @constant
+         */
+        USE_JQUERY_CSS: false,
+        /**
+         * Behaviour flag to deselect an item when its already selected
+         * @default true
+         * @type {bool}
+         * @constant
+         */
+        DESELECT_SELECTED: true,
+        /**
+         * Behaviour flag to clear selection when clicked on the container node
+         * @default true
+         * @type {bool}
+         * @constant
+         */
+        CLEAR_SELECTION_ON_CLICK: true,
+        /**
+         * Item actions
+         * @default true
+         * @type {object}
+         * @constant
+         */
+        ITEM_ACTIONS: {},
+        /**
+         * Grid actions (sort, hide column, layout)
+         * @default true
+         * @type {object}
+         * @constant
+         */
+        GRID_ACTIONS: {},
+        /**
+         * Publish selection change globally
+         * @default true
+         * @type {boolean}
+         * @constant
+         */
+        PUBLISH_SELECTION: false
+    };
+    /**
+     * Grid option keys
+     * @enum module:xgrid/types/GRID_OPTION
+     * @memberOf module:xgrid/types
+     */
+    types.GRID_OPTION = {
+        /**
+         * Instruct the grid to add jQuery theme classes
+         * @default true
+         * @type {string}
+         * @constant
+         */
+        USE_JQUERY_CSS: 'USE_JQUERY_CSS',
+        /**
+         * Behaviour flag to deselect an item when its already selected
+         * @default true
+         * @type {string}
+         * @constant
+         */
+        DESELECT_SELECTED: 'DESELECT_SELECTED',
+        /**
+         * Behaviour flag to deselect an item when its already selected
+         * @default true
+         * @type {string}
+         * @constant
+         */
+        CLEAR_SELECTION_ON_CLICK:'CLEAR_SELECTION_ON_CLICK',
+        /**
+         * Actions
+         * @default true
+         * @type {string}
+         * @constant
+         */
+        ITEM_ACTIONS:'ITEM_ACTIONS',
+        /**
+         * Actions
+         * @default true
+         * @type {string}
+         * @constant
+         */
+        GRID_ACTIONS:'GRID_ACTIONS'
+    };
+    /**
+     * All grid default features
+     * @enum module:xgrid/types/GRID_DEFAULT_FEATURES
+     * @memberOf module:xgrid/types
+     */
+    types.DEFAULT_GRID_FEATURES = {
+        SELECTION: {
+            CLASS: Selection,
+            IMPLEMENTATION: {},
+            CLASSES: null
+        },
+        KEYBOARD_SELECTION: {
+            CLASS: _GridKeyboardSelection,
+            IMPLEMENTATION: {},
+            CLASSES: null
+        },
+        COLUMN_HIDER: {
+            CLASS: ColumnHider,
+            IMPLEMENTATION: {},
+            CLASSES: null
+        }
+
+    };
+    /**
+     * All Grid Features for easy access
+     * @enum module:xgrid/types/GRID_FEATURES
+     * @memberOf module:xgrid/types
+     */
+    types.GRID_FEATURES = {
+        SELECTION: {
+            CLASS: Selection,
+            IMPLEMENTATION: {},
+            CLASSES: null
+        },
+        KEYBOARD_SELECTION: {
+            CLASS: _GridKeyboardSelection,
+            IMPLEMENTATION: {},
+            CLASSES: null
+        },
+        ACTIONS: {
+            CLASS: Actions,
+            IMPLEMENTATION: {},
+            CLASSES: null
+        },
+        CLIPBOARD:{
+            CLASS:Clipboard,
+            IMPLEMENTATION:{},
+            CLASSES:null
+        },
+        COLUMN_HIDER: {
+            CLASS: ColumnHider,
+            IMPLEMENTATION: {},
+            CLASSES: null
+        }
+    };
+    return declare(null,[],{});
+});
